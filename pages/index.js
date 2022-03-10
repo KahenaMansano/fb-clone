487edde60c0eef7870d3564ce1a9d5dedc4b60d3
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-03-10 11:21:02 -0400

    header

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+      <Header />
+
+      <main></main>
+    </div>
+  )
+}
